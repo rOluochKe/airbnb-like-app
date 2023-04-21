8f904fedbd35a4343d5fd239f5e1ad740e59c802
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-21 13:06:33 +0300

    steps 3,4,5, listing creation and favorite

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
   var cloudinary: any
 }
 
-const uploadPreset = "dxpgwg9u5";
+const uploadPreset = "iqxyacxs";
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
